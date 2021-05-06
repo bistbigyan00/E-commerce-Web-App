534f2f7a7930cc5dc9656ca06d4ffea3091141a9
bistbigyan00 <bist.bigyan@gmail.com>
2021-05-06 15:37:12 +1000

    update

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -23,7 +23,7 @@ MEDIA_DIR = os.path.join(BASE_DIR,'media')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%3357&v+gxd!objk=2wb91w_e3jx3=7ekjpzcdf@k4_u&qluzp'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
